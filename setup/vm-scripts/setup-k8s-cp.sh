33f32f49e6d458b94012eae3c1c1533e411d2d8f
Brendon Thiede <brendon.thiede@gmail.com>
2025-04-02 12:40:05 -0400

    Switching to using the hostname instead of default IP, since the IP is dynamic

--- setup/vm-scripts/setup-k8s-cp.sh
+++ setup/vm-scripts/setup-k8s-cp.sh
@@ -14,8 +14,27 @@ sudo chown $(id -u):$(id -g) $HOME/.kube/config
 # Install Flannel CNI
 kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
 
+# Fix etcd to not use the dynamic IP address (this will knock the cluster out for a bit)
+sudo sed -i \
+    -e 's|\-\-listen-peer-urls=.*|--listen-peer-urls=https://127.0.0.1:2380|' \
+    -e 's|\-\-listen-client-urls=.*|--listen-client-urls=https://127.0.0.1:2379|' \
+    -e 's|\-\-initial-advertise-peer-urls=.*|--initial-advertise-peer-urls=https://127.0.0.1:2380|' \
+    -e 's|\-\-initial-cluster=k8s-cp=.*|--initial-cluster=k8s-cp=https://127.0.0.1:2379|' \
+    -e 's|\-\-advertise-client-urls=.*|--advertise-client-urls=https://127.0.0.1:2380|' \
+    /etc/kubernetes/manifests/etcd.yaml
+
+# Give access to crictl
+sudo groupadd containerd
+sudo usermod -aG containerd $USER
+sudo chown root:containerd /run/containerd/containerd.sock
+sudo chmod 660 /run/containerd/containerd.sock
+sudo chown -R root:containerd /var/log/containers
+sudo chmod g+s /var/log/containers
+sudo chown -R root:containerd /var/log/pods
+sudo chmod g+s /var/log/pods
+
 # Install etcd tools
-ETCD_VERSION="$(kubectl exec -n kube-system etcd-k8s-cp -- etcdctl version | head -n1 | sed 's/.*: //')"
+ETCD_VERSION="$(sudo cat /etc/kubernetes/manifests/etcd.yaml | grep image: | sed -e 's|.*/etcd:||' -e 's|\-.*||')"
 curl -sSL https://github.com/etcd-io/etcd/releases/download/v${ETCD_VERSION}/etcd-v${ETCD_VERSION}-linux-amd64.tar.gz -o etcd.tar.gz
 tar xzf etcd.tar.gz
 sudo mv -f etcd-v${ETCD_VERSION}*/etcd* /usr/local/bin/
